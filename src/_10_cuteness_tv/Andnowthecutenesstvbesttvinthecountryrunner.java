844d3e482a4be59699a6bd5af2f77c2f72fc3a6e
League Student <league@iMac-21.attlocal.net>
2020-01-20 18:28:27 -0800

    week 6 2.0
    yep i did it again lol

--- src/_10_cuteness_tv/Andnowthecutenesstvbesttvinthecountryrunner.java
+++ src/_10_cuteness_tv/Andnowthecutenesstvbesttvinthecountryrunner.java
@@ -1,7 +1,7 @@
 package _10_cuteness_tv;
 
 public class Andnowthecutenesstvbesttvinthecountryrunner {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+
+	}
 }
