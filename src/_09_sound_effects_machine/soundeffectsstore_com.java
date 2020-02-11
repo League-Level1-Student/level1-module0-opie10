c055b1109f3a4311cbd8209d8d627a99290f81a6
League Student <league@iMac-21.attlocal.net>
2020-02-10 18:32:49 -0800

    week8
    done all of eclipse code for modue 0 working on worms of magic

--- src/_09_sound_effects_machine/soundeffectsstore_com.java
+++ src/_09_sound_effects_machine/soundeffectsstore_com.java
@@ -18,6 +18,8 @@ public class soundeffectsstore_com implements MouseListener, ActionListener {
 	JButton jb = new JButton();
 	JButton jb2 = new JButton();
 	JButton jb3 = new JButton();
+	JButton jb4 = new JButton();
+	JButton jb5 = new JButton();
 public void startsos() {
 	jf.setVisible(true);
 	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,6 +36,12 @@ jb2.addActionListener(this);
 jp.add(jb3);
 jb3.setText("PLUME FIRE DOOOOOOOM BOOOOOM!?!?!?!?!?!?!?!?!?!?");
 jb3.addActionListener(this);
+jp.add(jb4);
+jb4.setText("DOOM DEATH DOOM");
+jb4.addActionListener(this);
+jp.add(jb5);
+jb5.setText("DIE DOOM");
+jb5.addActionListener(this);
 jf.pack();
 
 	
@@ -86,7 +94,16 @@ public void actionPerformed(ActionEvent e) {
 		
 	}
   else if (e.getSource()==jb3) {
-	playSound("younotshall.mp3");
+	  Audio song= new Audio("younotshall.mp3");
+	song.play(Audio.PLAY_ENTIRE_SONG);
+}
+  else if (e.getSource()==jb4) {
+	  Audio Song= new Audio("TBGC.mp3");
+	Song.play(Audio.PLAY_ENTIRE_SONG);
+}
+  else if (e.getSource()==jb5) {
+	  Audio sonG= new Audio("tmmc.mp3");
+	sonG.play(Audio.PLAY_ENTIRE_SONG);
 }
 }
 @Override
